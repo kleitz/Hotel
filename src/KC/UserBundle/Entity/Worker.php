26e2952ac8b857dc3d20dbe786d2b1d3d6edc77f
KondzioRozpruwacz <KonradConer@gmail.com>
2014-10-14 14:40:47 +0200

    fix entity

--- src/KC/UserBundle/Entity/Worker.php
+++ src/KC/UserBundle/Entity/Worker.php
@@ -29,7 +29,7 @@ class Worker extends User
     private $workplace;
     
     /**
-    * @ORM\Column(name="comments", type="string", length=50)
+    * @ORM\Column(name="comments", type="string", length=500)
     */
     private $comments;
     
