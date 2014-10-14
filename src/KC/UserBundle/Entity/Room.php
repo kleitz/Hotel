0ee550f1cdc576a77b204722d79b3dd3ae922882
KondzioRozpruwacz <KonradConer@gmail.com>
2014-10-14 13:46:37 +0200

    Added Room Entity

---
+++ src/KC/UserBundle/Entity/Room.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace KC\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="room")
+ */
+class Room
+{
+    /**
+    * @var integer
+    *
+    * @ORM\Column(name="id", type="integer")
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+    private $id;
+    
+    /**
+     * @var integer
+     *     
+     * @ORM\Column(name="workerId", type="integer") 
+     */
+    private $workerId;
+    
+    /**
+     * @var integer
+     *     
+     * @ORM\Column(name="standardId", type="integer") 
+     */
+    private $standardId;
+    
+    
+    /**
+     * @var string
+     *     
+     * @ORM\Column(name="availability", type="string", length=1) 
+     */
+    private $availability;
+    
+    /**
+     * @var string
+     *     
+     * @ORM\Column(name="nrOfBeds", type="string", length=1) 
+     */
+    private $nrOfBeds;
+    
+    /**
+     * @var string
+     *     
+     * @ORM\Column(name="faultInformation", type="string", length=200) 
+     */
+    private $faultInformation;
+    
+    
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     * @return Room
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set workerId
+     *
+     * @param integer $workerId
+     * @return Room
+     */
+    public function setWorkerId($workerId)
+    {
+        $this->workerId = $workerId;
+
+        return $this;
+    }
+
+    /**
+     * Get workerId
+     *
+     * @return integer 
+     */
+    public function getWorkerId()
+    {
+        return $this->workerId;
+    }
+
+    /**
+     * Set standardId
+     *
+     * @param integer $standardId
+     * @return Room
+     */
+    public function setStandardId($standardId)
+    {
+        $this->standardId = $standardId;
+
+        return $this;
+    }
+
+    /**
+     * Get standardId
+     *
+     * @return integer 
+     */
+    public function getStandardId()
+    {
+        return $this->standardId;
+    }
+
+    /**
+     * Set availability
+     *
+     * @param string $availability
+     * @return Room
+     */
+    public function setAvailability($availability)
+    {
+        $this->availability = $availability;
+
+        return $this;
+    }
+
+    /**
+     * Get availability
+     *
+     * @return string 
+     */
+    public function getAvailability()
+    {
+        return $this->availability;
+    }
+
+    /**
+     * Set nrOfBeds
+     *
+     * @param string $nrOfBeds
+     * @return Room
+     */
+    public function setNrOfBeds($nrOfBeds)
+    {
+        $this->nrOfBeds = $nrOfBeds;
+
+        return $this;
+    }
+
+    /**
+     * Get nrOfBeds
+     *
+     * @return string 
+     */
+    public function getNrOfBeds()
+    {
+        return $this->nrOfBeds;
+    }
+
+    /**
+     * Set faultInformation
+     *
+     * @param string $faultInformation
+     * @return Room
+     */
+    public function setFaultInformation($faultInformation)
+    {
+        $this->faultInformation = $faultInformation;
+
+        return $this;
+    }
+
+    /**
+     * Get faultInformation
+     *
+     * @return string 
+     */
+    public function getFaultInformation()
+    {
+        return $this->faultInformation;
+    }
+}
