e3e0f0a3d2f4844b8996c459b2e5d4108f0f2102
KondzioRozpruwacz <KonradConer@gmail.com>
2014-10-14 15:17:28 +0200

    Added RoomList Entity

---
+++ src/KC/UserBundle/Entity/RoomList.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace KC\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="roomlist")
+ */
+class RoomList
+{
+    /**   
+     * @ORM\Column(name="bookingId", type="integer") 
+     */
+    private $bookingId;
+    
+     /**
+     * @ORM\Column(name="roomNumber", type="integer") 
+     */
+    private $roomNumber;
+    
+    /**
+    * @ORM\Column(name="price", type="decimal", precision=14, scale=2)
+    */
+    private $price;
+
+    /**
+     * Set bookingId
+     *
+     * @param integer $bookingId
+     * @return RoomList
+     */
+    public function setBookingId($bookingId)
+    {
+        $this->bookingId = $bookingId;
+
+        return $this;
+    }
+
+    /**
+     * Get bookingId
+     *
+     * @return integer 
+     */
+    public function getBookingId()
+    {
+        return $this->bookingId;
+    }
+
+    /**
+     * Set roomNumber
+     *
+     * @param integer $roomNumber
+     * @return RoomList
+     */
+    public function setRoomNumber($roomNumber)
+    {
+        $this->roomNumber = $roomNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get roomNumber
+     *
+     * @return integer 
+     */
+    public function getRoomNumber()
+    {
+        return $this->roomNumber;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return RoomList
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
