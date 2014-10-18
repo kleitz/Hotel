eace729eb5a7c1854b2e62c1fde51180325f0beb
KondzioRozpruwacz <KonradConer@gmail.com>
2014-10-18 15:36:42 +0200

    contact fix

--- src/KC/HotelBundle/Form/ContactType.php
+++ src/KC/HotelBundle/Form/ContactType.php
@@ -7,31 +7,35 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-public function contactAction(Request $request)
+
+class ContactType
 {
-    $defaultData = array('message' => 'Podaj swoje dane:');
-    $form = $this->createFormBuilder($defaultData)
-        ->add('sendSubject', 'text')
-        ->add('sendFrom', 'email')
-        ->add('sendBody', 'textarea')
-        ->add('send', 'submit')
-        ->getForm();
+    public function contactAction(Request $request)
+    {
+        $defaultData = array('message' => 'Podaj swoje dane:');
+        $form = $this->createFormBuilder($defaultData)
+            ->add('sendSubject', 'text')
+            ->add('sendFrom', 'email')
+            ->add('sendBody', 'textarea')
+            ->add('send', 'submit')
+            ->getForm();
 
-    $form->handleRequest($request);
+        $form->handleRequest($request);
 
-    if ($form->isValid()) {
-        // data is an array with "name", "email", and "message" keys
-        $data = $form->getData();
+        if ($form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+        }
+
+        // ... render the form
+        $message = \Swift_Message::newInstance()
+                ->setSubject( $data[0] )
+                ->setFrom( $data[1] )
+                ->setTo('KonradConer@gmail.com') 
+                ->setBody( $data[2] )
+            ;
+            $this->get('mailer')->send($message);
+
+            return new Response('contact');
     }
-
-    // ... render the form
-    $message = \Swift_Message::newInstance()
-            ->setSubject( $data[0] )
-            ->setFrom( $data[1] )
-            ->setTo('KonradConer@gmail.com') 
-            ->setBody( $data[2] )
-        ;
-        $this->get('mailer')->send($message);
-        
-        return new Response('contact');
 }
