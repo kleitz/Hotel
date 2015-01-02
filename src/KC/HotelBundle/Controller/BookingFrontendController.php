e6e0572c62bcc2d6e32c9a30f9feac6d3fb80248
tajpo <konrad.coner@chiave.pl>
2015-01-02 16:52:49 +0100

    reservation for user TODO:checking if avaibility

--- src/KC/HotelBundle/Controller/BookingFrontendController.php
+++ src/KC/HotelBundle/Controller/BookingFrontendController.php
@@ -14,8 +14,7 @@ use KC\HotelBundle\Entity\Booking;
  *
  * @Route("/booking")
  */
-class BookingFrontendController extends Controller
-{
+class BookingFrontendController extends Controller {
 
     /**
      * Lists all Booking entities.
@@ -24,8 +23,7 @@ class BookingFrontendController extends Controller
      * @Method("GET")
      * @Template()
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('KCHotelBundle:Booking')->findAll();
@@ -34,7 +32,6 @@ class BookingFrontendController extends Controller
             'entities' => $entities,
         );
     }
-      
 
     /**
      * Finds and displays a Booking entity.
@@ -43,8 +40,7 @@ class BookingFrontendController extends Controller
      * @Method("GET")
      * @Template()
      */
-    public function showAction($id)
-    {
+    public function showAction($id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('KCHotelBundle:Booking')->find($id);
@@ -53,100 +49,107 @@ class BookingFrontendController extends Controller
             throw $this->createNotFoundException('Unable to find Booking entity.');
         }
 
-        
+
 
         return array(
-            'entity'      => $entity,
-            
+            'entity' => $entity,
         );
     }
-    
-    
+
     /**
-    * Returns array with room availability based on room number and generates a form 
-    *
-    * @param Room $id
-    * 
-    * @Method({"GET", "POST"})
-    * @Template()  
-    * @Route("/{id}/availability", name="booking_room_availability")
-    */
-    public function checkAvailabilityAction(Request $request, $id)
-    {
+     * Returns array with room availability based on room number and generates a form
+     *
+     * @param Room $id
+     *
+     * @Method({"GET", "POST"})
+     * @Template()
+     * @Route("/{id}/availability", name="booking_room_availability")
+     */
+    public function checkAvailabilityAction(Request $request, $id) {
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('KCHotelBundle:Booking')->findByRoom($id);
 
-        if(!$entities)
-        {
-            return 0;
-        }
-        
+//        if (!$entities) {
+//            return $this->redirect($this->generateUrl('booking_form_availability'));
+//        }
+
         $results = array();
-        
-        foreach ($entities as $entity)
-        {
-            $results[$entity->getId()]=array(
-                'CheckIn'=>$entity->getCheckInDate(),
-                'CheckOut'=>$entity->getCheckOutDate(),
+
+        foreach ($entities as $entity) {
+            $results[$entity->getId()] = array(
+                'CheckIn' => $entity->getCheckInDate(),
+                'CheckOut' => $entity->getCheckOutDate(),
             );
         }
-        
+
         $booking = new Booking();
-        
+
         //ustanawianie danych ktore sie maja wpisac automatycznie
         $booking->setBookingDate(new \DateTime());
 
         //tworzenie formularza
         $form = $this->createFormBuilder($booking)
-            ->add('checkInDate', null, array(
-                'widget' => 'single_text',
-                'format' => 'yyyy-MM-dd',
-                'attr'=> array(
-                    'class' => 'dp',      
-                )
-            ))
-            ->add('checkOutDate', null, array(
-                'widget' => 'single_text',
-                'format' => 'yyyy-MM-dd',
-                'attr'=> array(
-                    'class' => 'dp',      
-                )
-            ))
-            ->add('save', 'submit', array('label' => 'Rezerwuj'))
-            ->getForm();
-        
-        if($request->isMethod('POST'))
-        {
+                ->add('checkInDate', null, array(
+                    'widget' => 'single_text',
+                    'format' => 'yyyy-MM-dd',
+                    'attr' => array(
+                        'class' => 'dp',
+                    )
+                ))
+                ->add('checkOutDate', null, array(
+                    'widget' => 'single_text',
+                    'format' => 'yyyy-MM-dd',
+                    'attr' => array(
+                        'class' => 'dp',
+                    )
+                ))
+                ->add('save', 'submit', array('label' => 'Rezerwuj'))
+                ->getForm();
+
+        if ($request->isMethod('POST')) {
             $form->handleRequest($request);
+            $dateIn = $booking->getCheckInDate();
+            $dateOut = $booking->getCheckOutDate();
+
+            $diff = date_diff($dateIn, $dateOut);
+            $diffDays = $diff->days;
+
+            $room = $em->getRepository('KCHotelBundle:Room')->find($id);
+            $booking->setRoom($room);
+            $price = (($room->getPrice()) * $diffDays);
+            $booking->setPrice($price);
+            $booking->setStatus('Booked');
+            $user = $this->getUser();
+            $booking->setClient($user);
+
 //            var_dump($booking);
 //            die;
-            
-        return $this->redirect($this->generateUrl('booking_form_availability'));
+
+            $em->persist($booking);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('booking'));
         }
-        
-        
+
+
         return array(
-            'availability'  =>  $results,
-            'form' =>$form->createView(),
+            'availability' => $results,
+            'form' => $form->createView(),
         );
-        
     }
-        
+
     /**
-    * Returns array with room availability based on room number
-    *
-    * @param Room $id
-    * 
-    * @Method("GET")
-    * @Template()  
-    * @Route("/availability/form", name="booking_form_availability")
-    */
-    public function checkBookingAction()
-    {
-        echo 'chuj';
-        
+     * Returns array with room availability based on room number
+     *
+     * @param Room $id
+     *
+     * @Method("GET")
+     * @Template()
+     * @Route("/availability/form", name="booking_form_availability")
+     */
+    public function checkBookingAction() {
+        echo 'siabada';
     }
-    
-    
+
 }
